7a80d7d74a86c45c34961fdb5e6cf130f1a4a02f
longvibol <vibollong@gmail.com>
2026-02-25 16:47:39 +0700

    df'

--- Softcode_Project/ResourceServer/src/main/java/com/vibol/api/ResourceServer/controllers/UsersController.java
+++ Softcode_Project/ResourceServer/src/main/java/com/vibol/api/ResourceServer/controllers/UsersController.java
@@ -1,15 +1,19 @@
 package com.vibol.api.ResourceServer.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
+@Slf4j
 public class UsersController {
 
     @GetMapping("/status/check")
     public String status(){
+        log.info("Request From scope profile");
         return "Working...";
     }
 }
+
