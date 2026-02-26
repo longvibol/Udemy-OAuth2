72ce90e7cd700ad85ebd8fd270c586649f34a3de
longvibol <vibollong@gmail.com>
2026-02-26 16:57:33 +0700

    fdf

--- Softcode_Project/ResourceServer/src/main/java/com/vibol/api/ResourceServer/controllers/UsersController.java
+++ Softcode_Project/ResourceServer/src/main/java/com/vibol/api/ResourceServer/controllers/UsersController.java
@@ -1,9 +1,9 @@
 package com.vibol.api.ResourceServer.controllers;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
@@ -14,6 +14,14 @@ public class UsersController {
     public String status(){
         log.info("Request From scope profile");
         return "Working...";
+    }
+
+    @PreAuthorize("hasAuthority('ROLE_developer')")
+//    @PreAuthorize("hasRole('developer')")
+//    @Secured("ROLE_developer")
+    @DeleteMapping(path="/{id}")
+    public String deletedUser(@PathVariable String id){
+        return "Deleted user with id: " + id;
     }
 }
 
