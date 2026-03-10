5248eb77f24f953962bea03838873b4205cdf819
longvibol <vibollong@gmail.com>
2026-03-10 16:49:13 +0700

    df

--- Softcode_Project/PhotoAppWebClient/src/main/java/com/vibol/clients/PhotoAppWebClient/controller/AlbumsController.java
+++ Softcode_Project/PhotoAppWebClient/src/main/java/com/vibol/clients/PhotoAppWebClient/controller/AlbumsController.java
@@ -2,56 +2,35 @@ package com.vibol.clients.PhotoAppWebClient.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
-import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import com.vibol.clients.PhotoAppWebClient.controller.response.AlbumRest;
 
 @Controller
 public class AlbumsController {
 
-    private final RestTemplate restTemplate;
-
-    public AlbumsController(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
-    }
+	
+	@Autowired
+	WebClient webClient;
 
     @GetMapping("/albums")
     public String getAlbums(
-            Model model,
-            @AuthenticationPrincipal OidcUser principal,
-            @RegisteredOAuth2AuthorizedClient("mywebclient") OAuth2AuthorizedClient authorizedClient) {
-
-        String accessToken = authorizedClient.getAccessToken().getTokenValue();
+            Model model,@AuthenticationPrincipal OidcUser principal) {
 
         String url = "http://localhost:8082/albums";
+        
+        List<AlbumRest> albums = webClient.get()
+        		.uri(url).retrieve().bodyToMono(new ParameterizedTypeReference<List<AlbumRest>>() {
+				}).block();
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setBearerAuth(accessToken);
-
-        HttpEntity<?> entity = new HttpEntity<>(headers);
-
-        ResponseEntity<List<AlbumRest>> responseEntity = restTemplate.exchange(
-                url,
-                HttpMethod.GET,
-                entity,
-                new ParameterizedTypeReference<List<AlbumRest>>() {}
-        );
-
-        List<AlbumRest> albumsList = responseEntity.getBody();
-        model.addAttribute("albums", albumsList);
-        model.addAttribute("username", principal.getPreferredUsername());
+        model.addAttribute("username", albums);
 
         return "albums";
     }
--- Softcode_Project/PhotoAppWebClient/src/main/java/com/vibol/clients/PhotoAppWebClient/security/SecurityConfig.java
+++ Softcode_Project/PhotoAppWebClient/src/main/java/com/vibol/clients/PhotoAppWebClient/security/SecurityConfig.java
@@ -1,5 +1,4 @@
 package com.vibol.clients.PhotoAppWebClient.security;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
